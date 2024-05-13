02877689304ae425e9df14af1ab4e97b9b273afb
Dwi Arifin <mr.dnarifin@gmail.com>
2024-05-13 14:39:58 +0800

    Update HR Job Ext

--- custom-addons/hr_job_ext/models/models.py
+++ custom-addons/hr_job_ext/models/models.py
@@ -6,14 +6,12 @@ from odoo import models, fields
 class Job(models.Model):
 
     _inherit = "hr.job"
-    cost_center = fields.Many2one("hr.cost_center", "Cost Center", required=True)
-    level = fields.Many2one("hr.level", "Job Level", required=True)
-    classification = fields.Many2one(
-        "hr.classification", "Job Classification", required=True
-    )
+    cost_center = fields.Many2one("hr.cost_center", "Cost Center")
+    level = fields.Many2one("hr.level", "Job Level")
+    classification = fields.Many2one("hr.classification", "Job Classification")
     section = fields.Many2one("hr.section", string="Section")
     level_hcs = fields.Char(string="Level HCS")
-    grade = fields.Many2one("hr.grade", string="Job Grade", required=True)
+    grade = fields.Many2one("hr.grade", string="Job Grade")
 
 
 #     @api.depends('value')
